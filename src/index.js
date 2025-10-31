dbd4c860d927ae83e4c4b445c7ff81727d852f3a
Mar <garciaprimomar@gmail.com>
2025-10-31 21:00:12 +0100

    guardar 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-como va la vaina
+// Después de instalar el paquete npm "colors", necesitamos importarlo (require)
+// en el archivo donde planeamos usarlo
+import myColors from 'colors/safe.js';
+
+console.log(myColors.yellow('¡Hola WebDev Women Valencia!'));
+console.log(myColors.magenta('Aprendiendo Node.js es divertido'));
+console.log(myColors.cyan.bold('Programar cambia vidas'));
+console.log(myColors.rainbow('WebDev Women Valencia'));
+console.log(myColors.green('¡Sigue aprendiendo!'));
