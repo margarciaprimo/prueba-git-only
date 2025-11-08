20fa8cd85806d7a664278145b4f5064d86f95b3b
Mar <garciaprimomar@gmail.com>
2025-11-09 00:15:42 +0100

    Ejercicio empresa, parodia Authory

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-// Después de instalar el paquete npm "colors", necesitamos importarlo (require)
-// en el archivo donde planeamos usarlo
-import myColors from 'colors/safe.js';
+// app.js
 
-console.log(myColors.yellow('¡Hola WebDev Women Valencia!'));
-console.log(myColors.magenta('Aprendiendo Node.js es divertido'));
-console.log(myColors.cyan.bold('Programar cambia vidas'));
-console.log(myColors.rainbow('WebDev Women Valencia'));
-console.log(myColors.green('¡Sigue aprendiendo!'));
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+import "dotenv/config";
+
+import express from "express";
+import { engine } from "express-handlebars";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Configurar carpeta public
+app.use(express.static("public"));
+
+// Configurar Handlebars
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+// ITERACIÓN 1: Implementar ruta GET / (home)
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
